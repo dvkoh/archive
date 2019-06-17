4d904ca95b8d158ce2b3d7889918aed7628f1178
dvkoh <topdavid@naver.com>
2019-06-17 17:01:09 +0900

    Updates

--- precache-manifest.c74ed8ba24d91b967c0fe7f780c2814c.js
+++ precache-manifest.c74ed8ba24d91b967c0fe7f780c2814c.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/archive/static/js/runtime~main.1a3f4e59.js"
   },
   {
-    "revision": "601d953ab249d4ac372e",
-    "url": "/archive/static/js/main.24b4494f.chunk.js"
+    "revision": "5358321c9b228bd83ca6",
+    "url": "/archive/static/js/main.a8bc0455.chunk.js"
   },
   {
     "revision": "2683c51af37346209a25",
     "url": "/archive/static/js/2.e0e0e208.chunk.js"
   },
   {
-    "revision": "601d953ab249d4ac372e",
+    "revision": "5358321c9b228bd83ca6",
     "url": "/archive/static/css/main.2b894095.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/archive/static/css/2.bb379bbf.chunk.css"
   },
   {
-    "revision": "53bcadc291538c038081ed1aa4ec4123",
+    "revision": "1d9e581469542073fa119e9ea417227c",
     "url": "/archive/index.html"
   }
 ];
